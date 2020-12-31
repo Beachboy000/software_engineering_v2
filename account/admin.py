0d745369e3e318450e0ea71f60407b09ac2db293
Beachboy000 <jack67660@gmail.com>
2020-12-31 17:56:35 +0800

    Account display

--- account/admin.py
+++ account/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from import_export.admin import ImportExportActionModelAdmin
 from .models import Account
 
+@admin.register(Account)
+class accountFilter(ImportExportActionModelAdmin):
+    #list_display = [field.name for field in Account._meta.fields] #display all account content
 
+    list_display = ('id','userName',)
+    list_filter = ('id',)
+    list_per_page = 10
+
+    #ordering = ('-mod_date',)
+    #list_filter = ('userName')
 # Register your models here.
-admin.site.register(Account)
+#admin.site.register(Account)
 
