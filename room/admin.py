86027125ed22e48dbe480b5d9b535f4f2f57aef0
Beachboy000 <jack67660@gmail.com>
2021-01-01 16:51:03 +0800

    delete building

--- room/admin.py
+++ room/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from import_export.admin import ImportExportActionModelAdmin
 from .models import Room
 
-
+@admin.register(Room)
+class accountFilter(ImportExportActionModelAdmin):
+    list_display = ('id',)
+    list_filter = ('id',)
+    list_per_page = 20
 # Register your models here.
-admin.site.register(Room)
+#admin.site.register(Room)
--- seq/admin.py
+++ seq/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from import_export.admin import ImportExportActionModelAdmin
 from .models import Seq
 
-
+@admin.register(Seq)
+class accountFilter(ImportExportActionModelAdmin):
+    list_display = ('id','userName',)
+    list_filter = ('id','user_run',)
+    list_per_page = 20
 # Register your models here.
-admin.site.register(Seq)
+#admin.site.register(Room)
